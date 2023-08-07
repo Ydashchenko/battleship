975b0966c243b2b94c0bd603ef022c74e714841a
madjonga <madjonga123@gmail.com>
2023-08-07 13:18:14 +0300

    Create Ship factory func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import _ from 'lodash'
 import './style.css'
 
+function Ship(length) {
+    return {
+        length,
+        howManyHits: 0,
+        beenSunk: false,
+        hit: () => {
+            howManyHits += 1
+        },
+        isSunk: () => {
+            if (howManyHits >= length) {
+                beenSunk = true
+            }
+        }
+
+    }
+}
