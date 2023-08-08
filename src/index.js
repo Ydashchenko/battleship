72bdb9e16b4b4127f75b5e1e76c5bf6e5f16500b
madjonga <madjonga123@gmail.com>
2023-08-08 15:00:40 +0300

    Add placeShip method

--- src/index.js
+++ src/index.js
@@ -20,3 +20,70 @@ export function Ship(length) {
     }
 }
 
+export function Gameboard() {
+    const boardSize = 10
+    const board = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null))
+
+    function checkShipOverlap(x, y, shipLength, isHorizontal) {
+        const start = isHorizontal ? x : y;
+        const end = start + shipLength;
+        for (let i = start; i < end; i++) {
+            if (isHorizontal) {
+                if (board[i][y]) return true;
+            } else {
+                if (board[x][i]) return true;
+            }
+        }
+        return false;
+    }
+
+    function placeShip(ship, x, y, isHorizontal) {
+        if ((isHorizontal && x + shipLength > boardSize) || (!isHorizontal && y + shipLength > boardSize)) {
+            throw new Error('Invalid coordinates. Ship placement out of bounds.');
+        }
+      
+        if (checkShipOverlap(x, y, shipLength, isHorizontal)) {
+            throw new Error('Invalid coordinates. Ship overlaps with another ship.');
+        }
+
+        for (cell in ship.length) {
+            if (isHorizontal) {
+                board[x + cell][y] = ship
+            } else {
+                board[x][y + cell] = ship
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    let battleShips = {
+        carrier: {
+            quantity: 1,
+            length: 4
+        },
+        destroyer: {
+            quantity: 2,
+            length: 3
+        },
+        submarine: {
+            quantity: 3,
+            length: 2
+        },
+        boat: {
+            quantity: 4,
+            length: 1
+        }
+    } */
